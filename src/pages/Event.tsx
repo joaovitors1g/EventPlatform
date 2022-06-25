e2f1d85cd18e53e8aec80eec38ebd62703253583
joaovitors1g <joao_vitor9846@hotmail.com>
2022-06-24 22:16:12 -0300

    feat: add event page content

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,15 @@
 import { Header } from '../components/Header';
+import { SideBar } from '../components/SideBar';
+import { Video } from '../components/Video';
 
 export function Event() {
-  return <Header />;
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='flex flex-1'>
+        <Video />
+        <SideBar />
+      </main>
+    </div>
+  );
 }
