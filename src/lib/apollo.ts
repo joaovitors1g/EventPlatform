778715f5a908f33d682dae95a19e9085c4a90f7f
joaovitors1g <joao_vitor9846@hotmail.com>
2022-06-28 20:53:22 -0300

    refactor: move sensitive content to .env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4t1io8g2xty01z69t5c3wud/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
